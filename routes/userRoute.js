c42aa31479f4f4d90949942ecdf2f400c390b877
unknown <aztoorabally7447@gmail.com>
2022-07-28 14:36:43 +0200

    'security'

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const con = require("../lib/db_connection");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Register Route
 // The Route where Encryption starts
@@ -26,8 +27,8 @@ router.post("/register", (req, res) => {
     let user = {
       full_name,
       email,
-      // We sending the hash value to be stored witin the table
-      hash,
+      
+      password:hash,
       user_type,
       phone,
       country,
@@ -46,7 +47,8 @@ router.post("/register", (req, res) => {
 
 
 // Login
-// The Route where Decryption happens
+
+// Login
 router.post("/login", (req, res) => {
   try {
     let sql = "SELECT * FROM users WHERE ?";
@@ -58,18 +60,38 @@ router.post("/login", (req, res) => {
       if (result.length === 0) {
         res.send("Email not found please register");
       } else {
-        // Decryption
-        // Accepts the password stored in database and the password given by user (req.body)
         const isMatch = await bcrypt.compare(
           req.body.password,
           result[0].password
         );
-        // If password does not match
         if (!isMatch) {
           res.send("Password incorrect");
-        }
-        else {
-          res.send(result)
+        } else {
+          // The information the should be stored inside token
+          const payload = {
+            user: {
+              user_id: result[0].user_id,
+              full_name: result[0].full_name,
+              email: result[0].email,
+              user_type: result[0].user_type,
+              phone: result[0].phone,
+              country: result[0].country,
+              billing_address: result[0].billing_address,
+              default_shipping_address: result[0].default_shipping_address,
+            },
+          };
+          // Creating a token and setting expiry date
+          jwt.sign(
+            payload,
+            process.env.jwtSecret,
+            {
+              expiresIn: "365d",
+            },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
         }
       }
     });
@@ -78,7 +100,20 @@ router.post("/login", (req, res) => {
   }
 });
 
-
+// Verify
+router.get("/verify", (req, res) => {
+  const token = req.header("x-auth-token");
+  jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
+    if (error) {
+      res.status(401).json({
+        msg: "Unauthorized Access!",
+      });
+    } else {
+      res.status(200);
+      res.send(decodedToken);
+    }
+  });
+});
 router.get("/", (req, res) => {
   try {
     con.query("SELECT * FROM users", (err, result) => {
@@ -90,6 +125,20 @@ router.get("/", (req, res) => {
     res.status(400).send(error);
   }
 });
+
+const middleware = require("../middleware/auth");
+
+  router.get("/", middleware, (req, res) => {
+    try {
+      let sql = "SELECT * FROM users";
+      con.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
   
 // Gets one users
 router.get("/:id", (req, res) => {
