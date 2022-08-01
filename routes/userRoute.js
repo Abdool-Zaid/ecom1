493fef6250de611d128018fa99e54ff0f7d950fb
unknown <aztoorabally7447@gmail.com>
2022-08-01 15:03:04 +0200

    'home'

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const con = require("../lib/db_connection");
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const fs = require('fs')
+const nodemailer = require('nodemailer');
+
 
 // Register Route
 // The Route where Encryption starts
@@ -234,6 +235,100 @@ router.delete("/:id", (req, res) => {
     console.log(error);
     res.status(400).send(error);
   }
+
+
 });
 
+router.post('/forgot-psw', (req, res) => {
+  try {
+  let sql = "SELECT * FROM users WHERE ?";
+  let user = {
+    email: req.body.email,
+  };
+  con.query(sql, user, (err, result) => {
+    if (err) throw err;
+    if(result === 0) {
+      res.status(400), res.send("Email not found")
+    }
+    else {
+
+      // Allows me to connect to the given email account || Your Email
+      const transporter = nodemailer.createTransport({
+        host: process.env.MAILERHOST,
+        port: process.env.MAILERPORT,
+        auth: {
+          user: process.env.MAILERUSER,
+          pass: process.env.MAILERPASS,
+        },
+      });
+
+      // How the email should be sent out
+    var mailData = {
+      from: process.env.MAILERUSER,
+      // Sending to the person who requested
+      to: result[0].email,
+
+      subject: 'Password Reset',
+      html:
+        `<div>
+          <h3>Hi ${result[0].full_name},</h3>
+          <br>
+          <marquee behavior="scroll" direction="right">
+          
+          <h4>Click link below to reset your password</h4>
+          </marquee>
+      
+          
+      
+
+          <a href="https://user-images.githubusercontent.com/4998145/52377595-605e4400-2a33-11e9-80f1-c9f61b163c6a.png">
+            Click Here to Reset Password
+            user_id = ${result[0].user_id}
+          </a>
+
+          <br>
+          <p>For any queries feel free to contact us...</p>
+          <div>
+            Email: ${process.env.MAILERUSER}
+            <br>
+          <div>
+        </div>`
+    };
+
+    // Check if email can be sent
+    // Check password and email given in .env file
+    transporter.verify((error, success) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email valid! ', success)
+      }
+    });
+
+    transporter.sendMail(mailData,  (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.status('Please Check your email', result[0].user_id)
+      }
+    });
+
+    }
+  });
+} catch (error) {
+  console.log(error);
+}
+})
+
+
+
+
+
+
+
+
+
 module.exports = router;
+
+
+
